c8cc44e7f2086d491091676834c06cc91e857d91
rakinafser-at-060170607040 <rakin@espressly.co>
2024-05-09 18:48:48 +0600

    :tada: implementation list in python

---
+++ list.py
@@ -0,0 +1,25 @@
+List = [1,2,2,4,4,"python",3.2,1]
+print(List)
+print(List[0])
+print(List[1])
+print(List[2])
+print(List[3])
+print(List[4])
+print(List[5])
+# print the last element of list
+print("Print Last element of List::::>>>",List[-1])
+    
+# print the third last element of list
+print("Print Third last element of List===>>>",List[-3])
+# reverse the list
+print("Reverse the list is:",List[::-1])
+# List operation
+
+#== Creating multidimensional list===
+
+multi_list = [[1,2,3],[4,5,6],[7,8,9]]
+print("Multidimensional list is:",multi_list)
+
+#== Accessing multidimensional list ===
+
+print("Accessing multidimensional list is:",multi_list[2][0])
